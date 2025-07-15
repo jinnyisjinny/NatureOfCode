b0c499edccab7fb35379cdd19afacf08bb992c7f
jinnyisjinny <jieuny@sogang.ac.kr>
2025-07-16 03:42:44 +0900

    add note

--- sketch.js
+++ sketch.js
@@ -50,6 +50,7 @@ class Walker {
     //   this.y--;
     // }
 
+    // Random Walks Exercise 1.3 - 50% chance of moving in the direction of mouse
     let prob = random(1);
     if (prob < 0.5) {
       let diffX = this.x - mouseX;
