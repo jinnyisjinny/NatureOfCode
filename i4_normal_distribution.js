983ed082152641f40f42c39f141211905f189f90
jinnyisjinny <jieuny@sogang.ac.kr>
2025-07-16 13:51:18 +0900

    normal distribution example

--- i4_normal_distribution.js
+++ i4_normal_distribution.js
@@ -1,7 +1,14 @@
 function i4_normal_distribution_setup() {
-
+    background(255);
 }
 
 function i4_normal_distribution_draw() {
-    
+    let num = randomGaussian();
+    let sd = 60;
+    let mean = width / 2;
+    let x = sd * num + mean;
+
+    noStroke();
+    fill(0, 10);
+    ellipse(x, height / 2, 16, 16);
 }
